80f77d2202dba564447955aeb9d54ed05921e581
hongocvan <van.hongoc2512@gmail.com>
2022-06-01 10:32:41 +0700

    Fix create API + Securing the API

--- backend/functions/create.js
+++ backend/functions/create.js
@@ -11,7 +11,7 @@ export const main = handler(async (event) => {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      userId: '123', // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
@@ -19,7 +19,7 @@ export const main = handler(async (event) => {
     },
   };
 
-  await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
   return params.Item;
 });
